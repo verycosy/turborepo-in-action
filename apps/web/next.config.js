f46936365f4f9deea813ae2b0a64cf5a49fb5367
Jinho Jang <verycosy@kakao.com>
2023-10-10 10:15:32 +0900

    next에 front 의존성을 추가하고 트랜스파일

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  transpilePackages: ['front'],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
