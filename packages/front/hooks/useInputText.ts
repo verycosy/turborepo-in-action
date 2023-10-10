d5d6da81f819aba03f773beb197e60a857aec0f1
Jinho Jang <verycosy@kakao.com>
2023-10-10 11:31:37 +0900

    useInputText 오류 수정

--- packages/front/hooks/useInputText.ts
+++ packages/front/hooks/useInputText.ts
@@ -3,8 +3,8 @@ import { useState, ChangeEventHandler } from 'react';
 export const useInputText = () => {
   const [text, setText] = useState('');
 
-  const onChange: ChangeEventHandler = (evt) => {
-    const value = evt.target.textContent ?? '';
+  const onChange: ChangeEventHandler<HTMLInputElement> = (evt) => {
+    const value = evt.target.value ?? '';
     setText(value);
   };
 
